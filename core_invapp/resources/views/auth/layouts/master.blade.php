fde0e5be25091788684f11082fd61c53c5a79796
Michael Kachi <jesmikky@gmail.com>
2022-08-07 02:24:42 +0100

    updates:

--- core_invapp/resources/views/auth/layouts/master.blade.php
+++ core_invapp/resources/views/auth/layouts/master.blade.php
@@ -46,16 +46,13 @@
 <script src="{{ asset('/assets/js/app.js') }}"></script>
 <!-- Start of LiveChat (www.livechat.com) code -->
 <script>
-    window._lc = window._lc || {};
+    window.__lc = window.__lc || {};
     window.__lc.license = 14388270;
-    ;(function(n,t,c){function i(n){return e.h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n._lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
 </script>
 <noscript><a href="https://www.livechat.com/chat-with/14388270/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a></noscript>
 <!-- End of LiveChat code -->
-@stack('scripts')
-@if(sys_settings('tawk_api_key'))
 
-@endif
 @if(sys_settings('footer_code'))
     {{ html_string(sys_settings('footer_code')) }}
 @endif

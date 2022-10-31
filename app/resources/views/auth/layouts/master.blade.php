1cdfa131f1c815405b7306f7bca4cac706221252
Michael Kachi <jesmikky@gmail.com>
2022-10-31 17:39:57 +0100

    updates

--- app/resources/views/auth/layouts/master.blade.php
+++ app/resources/views/auth/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title', 'Welcome') | {{ site_info('name') }}</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('/assets/css/apps.css?ver=1.1.0') }}">
 @if (!empty(global_meta_content('seo')))
     {!! global_meta_content('seo') !!}

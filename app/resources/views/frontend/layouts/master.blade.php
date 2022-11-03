fe23a25def67c2c667d54a1d3715fa590c1b6d73
Michael Kachi <jesmikky@gmail.com>
2022-11-03 13:40:20 +0100

    updates

--- app/resources/views/frontend/layouts/master.blade.php
+++ app/resources/views/frontend/layouts/master.blade.php
@@ -150,14 +150,15 @@ use App\Enums\UserRoles;
 
 
 <script type="text/javascript">
-    var _smartsupp = _smartsupp || {};
-    _smartsupp.key = '083302cfd8f6e17040e981b3e09637a72b5f551b';
-    window.smartsupp||(function(d) {
-        var s,c,o=smartsupp=function(){ o..push(arguments)};o.=[];
-        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-        c.type='text/javascript';c.charset='utf-8';c.async=true;
-        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-    })(document);
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/628d8ee4b0d10b6f3e73e28b/1g3sfcblt';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
     </script>
 @stack('scripts')
 
